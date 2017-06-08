074f91370e1e18de76e6f4932419ad3f0f4fda94
jstephani2 <jstephani2@wisc.edu>
2017-06-07 21:28:05 -0500

    Add files via upload

---
+++ Destination.java
@@ -0,0 +1,62 @@
+import java.util.ArrayList;
+
+public class Destination {
+	private String name;
+	private ArrayList<String> packingList;
+	private int numItemsInList;
+	private Destination next;
+	private double distance;
+	private int length;
+	
+	public Destination(String name, ArrayList<String> packingList, Destination next, double distance, int length)
+	{
+		this.name = name;
+		this.packingList = packingList;
+		this.next = next;
+		this.distance = distance;
+		this.length = length;
+	}
+	public Destination(String name, Destination next, double distance, int length)
+	{
+		this.name = name;
+		packingList = new ArrayList<String>();
+		this.next = next;
+		this.distance = distance;
+		this.length = length;
+	}
+	public void addToPackingList(String item)
+	{
+		packingList.add(item);
+		packingList.sort(null);
+	}
+	public ArrayList<String> getPackingList()
+	{
+		return packingList;
+	}
+	public void removeFromList(int index)
+	{
+		packingList.remove(index);
+	}
+	public void removeFromList(String item)
+	{
+		if(packingList.remove(item))
+			System.out.println(item + " successfully removed.");
+		else
+			System.out.println(item + " did not exist in the list.");
+	}
+	public void setNext(Destination place)
+	{
+		this.next = place;
+	}
+	public Destination getNext()
+	{
+		return next;
+	}
+	public void print()
+	{
+		System.out.println(name + " " + distance + " miles " + length + " minutes.");
+		if(null == next)
+			return;
+		next.print();
+	}
+}
---
+++ Main.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Destination origin = new Destination("home", null, 0, 0);
+		Destination tail = origin;
+		
+		Scanner in = new Scanner(System.in);
+		while(true)
+		{
+			System.out.println("(a)dd a new destination");
+			System.out.println("(p)rint out report");
+			System.out.println("(r)emove a destination");
+			System.out.println("add a packing (l)ist");
+			
+			String input = in.next();
+			switch(input)
+			{
+			case "a":
+				tail.setNext(addNewDestination(in, tail));
+				tail = tail.getNext();
+				break;
+			case "p":
+				origin.print();
+				break;
+			}
+			
+		}
+	}
+	private static Destination addNewDestination(Scanner in, Destination tail)
+	{
+		System.out.println("Enter Destination Name: ");
+		String name = in.next();
+		System.out.println("Distance from previous destination: ");
+		double distance = in.nextDouble();
+		System.out.println("Length from previous destination (in mins):");
+		int length = in.nextInt();
+		Destination newPlace = new Destination(name, null, distance, length);
+		return newPlace;
+	}
+
+}
