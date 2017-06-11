7d0902a7e82434a8838bf9ff68740e370bb25bb6
BenGiese22 <bengiese22@gmail.com>
2017-06-11 17:19:30 -0500

    GUI Addition 6/11/2017

--- NewTripController.java
+++ NewTripController.java
@@ -2,14 +2,14 @@
  * Created by bengi on 6/8/2017.
  */
 
-import java.net.URL;
-import java.util.ResourceBundle;
-
-import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.Pane;
+import javafx.scene.layout.HBox;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
 
 /**
  * Controller for the New Trip Window
@@ -18,58 +18,21 @@ import javafx.scene.layout.Pane;
  */
 public class NewTripController implements Initializable {
 
-    /*
-     * Get start layout y , when adding under it add the stop 20 under the start y, move the add stop down 20.
-     * and resize pane by 20. limit pane max size.
-     *
-     */
-
-    private int numOfStops = 1;
-
     //FXML import
-    public TextField startTextField;
-    public TextField addStopTextField;
-    public TextField endTextField;
-    public Pane pane;
-    public Label startLabel;
-    public Label addStopLabel;
-    public Label endLabel;
+
+    private ArrayList<Destination> destinationArrayList = new ArrayList<>();
 
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle bundle) {
-
+        HBox hBox = new HBox();
+        Label startLabel = new Label("Start: ");
+        TextField startTextField = new TextField();
+        //TODO Destination declaration and add to arraylist
     }
 
-    //create new stop
+    //HBox with label and TextBox inside.
 
-
-    public void addStopTextField(ActionEvent event) {
-        //pane resize
-        double sizeBeforeResize = pane.getHeight();
-        pane.setPrefHeight(sizeBeforeResize + 20);
-        //stop generation
-        Label label = new Label("Stop:");
-        label.setLayoutX(startLabel.getLayoutX());
-        label.setLayoutY(startLabel.getLayoutY() + 20 * numOfStops);
-        label.setText("Stop:");
-        TextField textField = new TextField();
-        textField.setLayoutX(startTextField.getLayoutX() - 5);
-        textField.setLayoutY(startTextField.getLayoutY() + 20);
-        textField.setText(addStopTextField.getText());
-        //move addstop and end
-        addStopLabel.setLayoutY(addStopLabel.getLayoutY() + 20);
-        addStopTextField.setLayoutY(addStopTextField.getLayoutY() + 20);
-        endLabel.setLayoutY(endLabel.getLayoutY() + 20);
-        endTextField.setLayoutY(endTextField.getLayoutY() + 20);
-        //update
-        numOfStops++;
-        //if entered, then pane size y + 20, addstop and end moved down 20.
-    }
-
-    public void endTextField(ActionEvent event) {
-
-    }
 }
